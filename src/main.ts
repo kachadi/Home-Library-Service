2ec727c2887c73a51fe36799d394c6aaff521c6f
kachadi <kachadiii@gmail.com>
2023-02-06 17:30:00 +0200

    refactor: refactored the Open API api.yaml file

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 dotenv.config();
@@ -14,6 +18,13 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const document = yaml.load(
+    readFileSync(join(__dirname, '..', 'doc', 'api.yaml'), 'utf8'),
+  ) as OpenAPIObject;
+
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
